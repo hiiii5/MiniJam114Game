7dcbd90a5f1b891aba033cebefa2c8159b54bc6c
dustin roden <mrhiiii16@gmail.com>
2022-09-01 23:21:58 -0500

    Added project files.

---
+++ LittleUtopia/Source/LittleUtopia/LittleUtopia.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LittleUtopia.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LittleUtopia, "LittleUtopia" );
